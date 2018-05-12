907905236650a8a3d419f66fd40a5eb85680ace2
Guillaume Le Biller <guillaume.lebiller@gmail.com>
2018-05-12 21:37:50 +0200

    Added pixi.js to fix performance issue.

--- script.js
+++ script.js
@@ -1,53 +1,61 @@
-var currentWidth = document.body.offsetWidth;
-var width = 800;
-var height = 400;
+var currentWidth = document.body.clientWidth;
+var currentHeight = document.body.clientHeight;
+var DAY_DURATION = 2000;
+var SECOND_TO_DAY_RATIO = 60 * 60 * 24;
 
 var svg = d3.select("body")
     .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    /*.attr("preserveAspectRatio", "xMidYMid")
-    .attr("viewBox", "0 0 " + width + " " + height)
+    .attr("class", "map")
     .attr("width", currentWidth)
-    .attr("height", currentWidth * height / width)*/;
+    .attr("height", currentHeight);
 
-//Container for the gradients
-var defs = svg.append("defs");
-
-//Filter for the outside glow
-var filter = defs.append("filter")
-    .attr("id","glow");
-filter.append("feGaussianBlur")
-    .attr("stdDeviation","10")
-    .attr("result","coloredBlur");
-var feMerge = filter.append("feMerge");
-feMerge.append("feMergeNode")
-    .attr("in","coloredBlur");
-feMerge.append("feMergeNode")
-    .attr("in","SourceGraphic");
-
-    var projection = d3.geoMercator()
-        .scale(150)
-        .translate([width / 2, height / 1.41]);
+var projection = d3.geoMercator()
+    .scale(Math.round(100 * (currentHeight / 465))) // TODO also handle height > width style ratio
+    .translate([currentWidth / 2, currentHeight / 1.5]);
 
 var path = d3.geoPath()
     .pointRadius(2)
     .projection(projection);
 
+var app = new PIXI.Application({
+    width: currentWidth,
+    height: currentHeight,
+    transparent: true
+});
+app.view.className = 'flights';
+document.body.appendChild(app.view);
 
-/*var url = "http://enjalot.github.io/wwsd/data/world/world-110m.geojson";
-d3.json(url, function(err, geojson) {
-    svg.append("path")
-        .attr("d", path(geojson))
-});*/
+var points = [];
+var ticker = new PIXI.ticker.Ticker();
+
+var realTime = 0;
+var animationTime = 0;
+ticker.add(function (deltaTime) {
+    realTime += (deltaTime / 100);
+    animationTime = realTime;
+    d3.select(".date").text(animationTime);
+
+    for (var i = points.length - 1; i >= 0; i--) {
+        var point = points[i];
+        if (point.visible) {
+            if (point.data.delay + point.data.duration >= animationTime) {
+                // point.visible = false;
+                // points.splice(i, 1);
+            }
+            // var newPosition = point.data.route(time / 100.0);
+            // point.x = newPosition[0];
+            // point.y = newPosition[1];
+        } else if (point.data.delay >= animationTime) {
+            point.visible = true;
+        }
+    }
+});
 
 function delta(path) {
     var l = path.getTotalLength();
-    return function(i) {
-        return function(t) {
-            var p = path.getPointAtLength(t * l);
-            return "translate(" + p.x + "," + p.y + ")";
-        }
+    return function (t) {
+        var p = path.getPointAtLength(t * l);
+        return [p.x, p.y];
     }
 }
 
@@ -78,33 +86,32 @@ function loaded(error, countries, airports, flights) {
         })
         .attr("d", path);*/
 
-    var dayDuration = 2000;
-    var animationDuration = 31 * dayDuration;
+    var animationDuration = 31 * DAY_DURATION;
     var dateInterpolation = d3.interpolateNumber(0, 31);
     var startDate = moment("2018-04-01");
 
     d3.select(".date")
         .transition()
         .duration(animationDuration)
-        .on("start", function() {
+        .on("start", function () {
             d3.active(this)
-                .tween("text", function() {
+                .tween("text", function () {
                     var that = d3.select(this);
-                    return function(t) {
+                    return function (t) {
                         var dayAdded = Math.round(dateInterpolation(t));
-                        that.text(moment(startDate).add(dayAdded, 'days').format('DD/MM/YYYY'));
+                        //that.text(moment(startDate).add(dayAdded, 'days').format('DD/MM/YYYY'));
                     };
                 })
                 .ease(d3.easeLinear);
         });
 
-    for (var i = 0; i < flights.length; i++) {
+    for (var i = 0; i < /*flights.length*/ 20; i++) {
         var flight = flights[i];
 
         var flightStart = moment(flight['BEGIN_DATE']);
         var flightEnd = moment(flight['END_DATE']);
-        var delay = flightStart.diff(startDate, 'seconds') * dayDuration / (24 * 60 * 60);
-        var duration = flightEnd.diff(flightStart, 'seconds') * dayDuration / (24 * 60 * 60);
+        var delay = flightStart.diff(startDate, 'seconds') / SECOND_TO_DAY_RATIO;
+        var duration = flightEnd.diff(flightStart, 'seconds') / SECOND_TO_DAY_RATIO;
         if (delay < 0) {
             // flight start is before animation start
             continue;
@@ -120,6 +127,26 @@ function loaded(error, countries, airports, flights) {
             .datum({type: "LineString", coordinates: [origin, destination]})
             .attr("class", "route")
             .attr("d", path);
+        var routeFunction = delta(route.node());
+        var firstPoint = routeFunction(0);
+
+        var circle = new PIXI.Graphics();
+        circle.beginFill(0x9966FF);
+        circle.drawCircle(0, 0, 3);
+        circle.endFill();
+        circle.visible = false;
+        circle.x = firstPoint[0];
+        circle.y = firstPoint[1];
+        circle.data = {};
+        circle.data.delay = delay;
+        circle.data.duration = duration;
+        circle.data.route = routeFunction;
+        app.stage.addChild(circle);
+        console.log(flight);
+        console.log(circle.data);
+        points.push(circle);
+
+        /*
         var plane = svg.append("circle")
             .attr("class", "plane")
             .attr("r", 2)
@@ -136,27 +163,10 @@ function loaded(error, countries, airports, flights) {
             .attrTween("transform", delta(route.node()))
             .transition()
             .duration(500)
-            .remove();
+            .remove();*/
     }
 
-    /*for (var i = 0; i < 1000; i++) {
-        var start = Math.round(Math.random() * airportsFeatures.length);
-        var end = Math.round(Math.random() * airportsFeatures.length);
-
-        var route = svg.append("path")
-            .datum({type: "LineString", coordinates: [airportsFeatures[start].geometry.coordinates, airportsFeatures[end].geometry.coordinates]})
-            .attr("class", "route")
-            .attr("d", path);
-        var plane = svg.append("circle")
-            .attr("class", "plane")
-            .attr("r", 2)
-            .style("filter", "url(#glow)");
-
-        plane.transition()
-            .delay(Math.round(Math.random() * 10000))
-            .duration(5000)
-            .attrTween("transform", delta(route.node()));
-    }*/
+    ticker.start();
 }
 
 d3.queue()
